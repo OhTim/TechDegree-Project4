106aa3da531361774417113f82203c77e9b6faa1
Tim Dalton <timdaltonm@gmail.com>
2017-06-19 17:38:52 -0500

    Updated Search Input

--- js/main.js
+++ js/main.js
@@ -2,7 +2,20 @@ lightbox.option({
   wrapAround: true,
   resizeDuration: 100,
   showImageNumberLabel: false,
-  maxWidth: 1000,
-  maxHeight: 1000,
+  maxWidth: 800,
   disableScrolling: true
+});
+
+$("input[type='search']").on("keyup", function() {
+    var search = this.value.toUpperCase();
+
+    $("#gallery li").each(function(i) {
+      var caption = $(this).children().attr("data-title").toUpperCase();
+
+      if (caption.indexOf(search) > -1) {
+          $(this).css('display', 'initial');
+      } else {
+          $(this).css('display', 'none');
+      }
+    });
 });
